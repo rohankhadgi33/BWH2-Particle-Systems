201681f87b27ee7f1f18a112c160173d6761916b
Thao Lai <83465146+ThaoLai19@users.noreply.github.com>
2023-04-28 16:41:45 -0400

    Update drawmap.js

--- drawmap.js
+++ drawmap.js
@@ -1,11 +1,25 @@
-function drawMap() {
+/*function drawMap() {
     // Double forloop to draw
-    for (let x = array2d.length - 1; x >=0 ; x--) {
+    for (let x = 0; x < array2d.length; x--) {
         for (let y = 0; y < array2d[0].length; y++) {
             if (array2d[x][y] >= THRESHOLD) {
                 // Draw the rectangle
                 cvs.fillStyle = 'rgba(255, 255, 255)';
                 cvs.fillRect(x, y, 1, 1);
+            }
+        }
+    }
+}*/
+function drawMap() {
+    // Double forloop to draw
+    for (let x = array2d.length - 1; x >= 0 ; x--) {
+        for (let y = 0; y < array2d[0].length; y++) {
+            if (array2d[x][y] >= THRESHOLD) {
+                // Draw the rectangle
+                cvs.fillStyle = 'rgba(255, 255, 255)';
+                //cvs.fillRect(x, y, 1, 1);
+                cvs.fillRect(y, x, 1, 1);
+                // Phai rotate cai distance field de no ko anh huong toi clicking particle
             }
         }
     }
