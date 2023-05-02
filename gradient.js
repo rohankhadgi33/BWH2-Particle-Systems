bd62851e346de2d49ca84411d2270c2dd58e3d03
Thao Lai <83465146+ThaoLai19@users.noreply.github.com>
2023-05-01 23:34:21 -0400

    Update gradient.js

--- gradient.js
+++ gradient.js
@@ -5,36 +5,36 @@
 // row/edge is repeated so as to give the calculation a value that is not out
 // of bounds.
 function gradient() {
-    if (array2d != null) {
-        for (let x = 0; x < array2d.length; x++) {
+    if (array != null) {
+        for (let x = 0; x < array.length; x++) {
             gradient2d[x] = [];
-            for (let y = 0; y < array2d[x].length; y++) {
+            for (let y = 0; y < array[x].length; y++) {
                 if ((x == 0) && (y == 0)) {
                     // doing y - 1 and x - 1 here would be out of bounds
-                    gradient2d[x][y] = ((array2d[x][y + 1] - array2d[x][y]) / 2) + ((array2d[x + 1][y] - array2d[x][y]) / 2);
-                } else if ((x == array2d.length - 1) && (y == 0)) {
+                    gradient2d[x][y] = ((array[x][y + 1] - array[x][y]) / 2) + ((array[x + 1][y] - array[x][y]) / 2);
+                } else if ((x == array.length - 1) && (y == 0)) {
                     // doing x + 1 and y - 1 here would be out of bounds
-                    gradient2d[x][y] = ((array2d[x][y + 1] - array2d[x][y]) / 2) + ((array2d[x][y] - array2d[x - 1][y]) / 2);
-                } else if ((x == 0) && (y == array2d[x].length - 1)) {
+                    gradient2d[x][y] = ((array[x][y + 1] - array[x][y]) / 2) + ((array[x][y] - array[x - 1][y]) / 2);
+                } else if ((x == 0) && (y == array[x].length - 1)) {
                     // doing x - 1 and y + 1 here would be out of bounds
-                    gradient2d[x][y] = ((array2d[x][y] - array2d[x][y - 1]) / 2) + ((array2d[x + 1][y] - array2d[x][y]) / 2);
-                } else if ((x == array2d.length - 1) && (y == array2d[x].length - 1)) {
+                    gradient2d[x][y] = ((array[x][y] - array[x][y - 1]) / 2) + ((array[x + 1][y] - array[x][y]) / 2);
+                } else if ((x == array.length - 1) && (y == array[x].length - 1)) {
                     // doing x + 1 and y + 1 here would be out of bounds
-                    gradient2d[x][y] = ((array2d[x][y] - array2d[x][y - 1]) / 2) + ((array2d[x][y] - array2d[x - 1][y]) / 2);
+                    gradient2d[x][y] = ((array[x][y] - array[x][y - 1]) / 2) + ((array[x][y] - array[x - 1][y]) / 2);
                 } else if (x == 0) {
                     // doing x - 1 here would be out of bounds
-                    gradient2d[x][y] = ((array2d[x][y + 1] - array2d[x][y - 1]) / 2) + ((array2d[x + 1][y] - array2d[x][y]) / 2);
-                } else if (x == array2d.length - 1) {
+                    gradient2d[x][y] = ((array[x][y + 1] - array[x][y - 1]) / 2) + ((array[x + 1][y] - array[x][y]) / 2);
+                } else if (x == array.length - 1) {
                     // doing x + 1 here would be out of bounds
-                    gradient2d[x][y] = ((array2d[x][y - 1] - array2d[x][y + 1]) / 2) + ((array2d[x][y] - array2d[x - 1][y]) / 2);
+                    gradient2d[x][y] = ((array[x][y - 1] - array[x][y + 1]) / 2) + ((array[x][y] - array[x - 1][y]) / 2);
                 } else if (y == 0) {
                     // doing y - 1 here would be out of bounds
-                    gradient2d[x][y] = ((array2d[x][y + 1] - array2d[x][y]) / 2) + ((array2d[x + 1][y] - array2d[x - 1][y]) / 2);
-                } else if (y == array2d[x].length - 1) {
+                    gradient2d[x][y] = ((array[x][y + 1] - array[x][y]) / 2) + ((array[x + 1][y] - array[x - 1][y]) / 2);
+                } else if (y == array[x].length - 1) {
                     // doing y + 1 here would be out of bounds
-                    gradient2d[x][y] = ((array2d[x][y] - array2d[x][y - 1]) / 2) + ((array2d[x + 1][y] - array2d[x - 1][y]) / 2);
+                    gradient2d[x][y] = ((array[x][y] - array[x][y - 1]) / 2) + ((array[x + 1][y] - array[x - 1][y]) / 2);
                 } else {
-                    gradient2d[x][y] = ((array2d[x][y + 1] - array2d[x][y - 1]) / 2) + ((array2d[x + 1][y] - array2d[x - 1][y]) / 2);
+                    gradient2d[x][y] = ((array[x][y + 1] - array[x][y - 1]) / 2) + ((array[x + 1][y] - array[x - 1][y]) / 2);
                 }
                 console.log('ok');
             }
