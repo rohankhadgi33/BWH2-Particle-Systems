714cb89cf224e55ccb75bdd21c48c09e662e1e46
Thao Lai <83465146+ThaoLai19@users.noreply.github.com>
2023-05-01 23:20:22 -0400

    Update drawmap.js

--- drawmap.js
+++ drawmap.js
@@ -10,7 +10,8 @@
         }
     }
 }*/
-function drawMap() {
+
+/*function drawMap() {
     // Double forloop to draw
     for (let x = array2d.length - 1; x >= 0 ; x--) {
         for (let y = 0; y < array2d[0].length; y++) {
@@ -20,6 +21,31 @@ function drawMap() {
                 //cvs.fillRect(x, y, 1, 1);
                 cvs.fillRect(y, x, 1, 1);
                 // Phai rotate cai distance field de no ko anh huong toi clicking particle
+            }
+        }
+    }
+}*/
+
+function drawMap() {  
+    const rows = array2d[0].length;
+    const cols = array2d.length;
+    array = new Array(rows);
+    
+    for (let i = 0; i < rows; i++)
+    {
+      array[i] = new Array(cols);
+      for (let j = 0; j < cols; j++)
+      {
+        array[i][j] = array2d[j][i];  
+      }
+    }
+    // Double forloop to draw
+    //var n = 10;
+    for (let x = 0; x < array.length; x++) {
+        for (let y = 0; y < array[0].length; y++) {
+            if (array[x][y] >= THRESHOLD) {  
+                cvs.fillStyle = 'rgba(255, 255, 255)';
+                cvs.fillRect(x, y, 1, 1);
             }
         }
     }
